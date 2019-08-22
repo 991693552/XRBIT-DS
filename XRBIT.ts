7f7e8d2f7d451f584de6d783cbf98183c10b9a0a
XiaoRGEEK <service@xiaorgeek.com>
2019-08-22 15:33:09 +0800

    HC-SR04

--- XRBIT.ts
+++ XRBIT.ts
@@ -112,21 +112,15 @@ namespace XRbit_传感器 {
     //% blockGap=10
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
     export function Ultrasonic(Trig: DigitalPin, Echo: DigitalPin): number {
-        // send pulse
-        let list:Array<number> = [0, 0, 0, 0, 0];
-        for (let i = 0; i < 5; i++) {
-            pins.setPull(Trig, PinPullMode.PullNone);
-            pins.digitalWritePin(Trig, 0);
-            control.waitMicros(2);
-            pins.digitalWritePin(Trig, 1);
-            control.waitMicros(15);
-            pins.digitalWritePin(Trig, 0);
+        pins.setPull(Trig, PinPullMode.PullNone);
+        pins.digitalWritePin(Trig, 0);
+        control.waitMicros(5);
+        pins.digitalWritePin(Trig, 1);
+        control.waitMicros(15);
+        pins.digitalWritePin(Trig, 0);
 
-            let d = pins.pulseIn(Echo, PulseValue.High, 43200);
-            list[i] = Math.floor(d / 40)
-        }
-        list.sort();
-        let length = (list[1] + list[2] + list[3])/3;
+        let d = pins.pulseIn(Echo, PulseValue.High, 43200);
+        let length = Math.floor(d / 40)
         return  Math.floor(length);
     }
 }
